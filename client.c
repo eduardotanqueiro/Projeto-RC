db3f841c65cf97b5825ccf3513caf25986c79d3e
FSTUC <101585159+fstuc@users.noreply.github.com>
2022-04-16 16:03:08 +0100

    Split into different files

---
+++ client.c
@@ -0,0 +1,86 @@
+#include <header.h>
+
+void wait_clients(){
+   
+    int client_fd;
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size;
+    int client_number = 0;
+
+    fd_set read_set;
+    FD_ZERO(&read_set);
+    FD_SET(fd_bolsa,&read_set);
+
+    signal(SIGINT,sigint_client);
+
+    while(1){
+
+        while(waitpid(-1,NULL,WNOHANG)>0);
+
+        printf("[CLIENT] SERVER: Waiting for clients\n");
+        if( select(fd_bolsa+1,&read_set,NULL,NULL,NULL) > 0){
+            printf("[CLIENT] SERVER: Client Connected\n");
+
+            //Connection from some user
+            if( FD_ISSET(fd_bolsa,&read_set)){
+                
+                printf("[CLIENT] SERVER: A entrar no client\n");
+                client_addr_size = sizeof(client_addr_size);
+                
+                //Accept the TCP connection
+                client_fd = accept(fd_bolsa,(struct sockaddr *)&client_addr,&client_addr_size);
+                printf("[CLIENT] SERVER: Client aceite\n");
+
+                if(client_number == 10){
+                    printf("[CLIENT] SERVER: Reached maximum number of clients!\n");
+                }
+                else if( (childs_pids[client_number++] = fork()) == 0){
+                    close(fd_bolsa);
+                    close(fd_config);
+                    handle_client(client_fd);
+                    close(client_fd);
+                    exit(0);
+                }
+
+                close(client_fd); //???
+            }
+
+            printf("[MAIN] Looping\n");
+
+        }
+
+    }
+
+}
+
+void sigint_client(){
+
+    //clear client processes
+    for(int i = 0;i<10;i++){
+        kill(SIGINT,childs_pids[i]);
+    }
+    //clear client socket ??
+}
+
+void handle_client(int fd){
+
+    char buffer[BUFSIZ];
+    int n = -1;
+
+    //  TODO
+    //  -LOGIN
+    //  -MENU
+
+    do{
+        memset(buffer,0,BUFSIZ);
+        n = read(fd,buffer,BUFSIZ);
+        
+        if(n == 0)
+            break;
+
+        buffer[strlen(buffer)-1] = 0;
+        printf("[CLIENT] USER %d: %s\n",fd,buffer);
+    }while( strcmp( buffer, "SAIR") != 0);
+
+    printf("[CLIENT] Closing client\n");
+}
---
+++ header.h
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#include <sys/shm.h>
+#include <sys/ipc.h>
+
+void init(int porto_bolsa, int porto_config, char* config_file);
+void handle_client(int fd);
+int handle_admin();
+
+int add_user(char* args,struct sockaddr addr);
+int delete_user(char* args,struct sockaddr addr);
+int list(struct sockaddr addr);
+int refresh(char* args,struct sockaddr addr);
+void wait_clients();
+void sigint_client();
+
+
+void erro(char *s) {
+	perror(s);
+	exit(1);
+}
+
+#define INITIAL_REFRESH_TIME 2
+
+int fd_bolsa,fd_config;
+struct sockaddr_in addr_bolsa;
+struct sockaddr_in addr_config;
+
+pid_t childs_pids[10];
+pid_t admin_pid;
+
+typedef struct{
+    char username[31];
+    char password[31];
+    int balance;
+}user;
+
+
+typedef struct{
+    char name[31];
+    int value;
+}stock;
+
+typedef struct{
+    char name[31];
+    stock stock_list[3];
+
+}market;
+
+
+int refresh_time;
+int number_users;
+user users_list[10];
+user admin;
+market* market_list;
+int shmid;
